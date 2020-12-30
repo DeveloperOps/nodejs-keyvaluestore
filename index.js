deded240bdaad952d8dc40cef198cd9f69f57a5a
DeveloperOps <manidecp@gmail.com>
2020-12-31 02:34:15 +0530

    did basic setup with each function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const keystore = require('./keyvalue');
 //i'm following the singleton approach
 keystore.instance();
+keystore.read("fod")
--- keyvalue.js
+++ keyvalue.js
@@ -1,23 +1,61 @@
 const fs = require('fs');
 const move = require('move-file');
 const sizeOf = require('object-sizeof');
+const dotenv = require('dotenv');
+const jsonfile = require('jsonfile');
+dotenv.config();
+
 
 const keystore = {
   instance: (path = "/keystore/") => {
-  fs.appendFile("keystore.json" , '' , (err) => {
-    if(err) throw err;
-    move("keystore.json" , __dirname +`${path}keystore.json`)
-    .then(()=>{ console.log(`File created at ${path}`) })
-    .catch(err => { throw err });
-  });
+  if(process.env.STORE === undefined) {
+      fs.appendFile("keystore.json" , {} , (err) => {
+        if(err) throw err;
+        move("keystore.json" , __dirname +`${path}keystore.json`)
+        .then(()=>{ 
+          fs.appendFile(".env" , `STORE = ${__dirname}${path}keystore.json` , (err) => {
+            if(err) throw err;
+            console.log("File created!!");
+          });
+        })
+        .catch(err => { throw err });
+      });
+    }
   },
   create: (key , value) => {
     if(key.length > 32) throw new Error("Key must be smaller than 32 chars");
     if(sizeOf(value) > 128000) throw new Error("Value must be less than 16KB in size");
-    const object = {};
-    object[key] = JSON.stringify(value);
-    console.log(JSON.stringify(object));
-  } 
+    fs.readFile(`${__dirname}/keystore/keystore.json` , (err , data) => {
+      if(err) throw err;
+      const json = (JSON.parse(data));
+      json[key] = value;
+      fs.writeFile(`${__dirname}/keystore/keystore.json` , JSON.stringify(json) , (err) => {
+        if(err) throw err;
+        console.log("Written");
+      })
+    })
+  },
+  read: (key) => {
+    if(key.length > 32) throw new Error("Key must be smaller than 32 chars");
+    fs.readFile(`${__dirname}/keystore/keystore.json` , (err , data) => {
+      if(err) throw err;
+      const json = (JSON.parse(data));
+      if(json[key]) console.log(json[key])
+      else console.log("Not found!!");
+    })
+  },
+  delete: (key) => {
+    if(key.length > 32) throw new Error("Key must be smaller than 32 chars");
+    fs.readFile(`${__dirname}/keystore/keystore.json` , (err , data) => {
+      if(err) throw err;
+      const json = (JSON.parse(data));
+      json[key] = value;
+      fs.writeFile(`${__dirname}/keystore/keystore.json` , JSON.stringify(json) , (err) => {
+        if(err) throw err;
+        console.log("Written");
+      })
+    })
+  }
 };
   
 module.exports = keystore;
