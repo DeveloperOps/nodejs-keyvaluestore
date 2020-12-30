0513bd38875a2c2f01d720e37e90587cdde3afbc
DeveloperOps <manidecp@gmail.com>
2020-12-30 23:27:03 +0530

    Initial file logic

--- keyvalue.js
+++ keyvalue.js
@@ -1,13 +1,22 @@
 const fs = require('fs');
+const move = require('move-file');
 
-const move = () => {
-  
-}
+/**
+ * @param {String} path
+ * Optional: please specify location ie /store/ 
+ * Default: /projectRoot/keystore/
+ */
+const keystore = (path = "/keystore/") => {
+    fs.appendFile("keystore.json" , '' , (err) => {
+      if(err) throw err;
+      move("keystore.json" , __dirname +`${path}keystore.json`)
+      .then(()=>{ console.log(`File created at ${path}`) })
+      .catch(err => { throw err });
+    });
 
-const keystore = (path = "./keystore/") => {
-  fs.appendFile(`keystore.json` , '' , (err)  => {
-    if(err) throw err;
-    console.log(`File created at ${path}`);
-  })
+    const create = () => {
+      
+    }
+
 }
 module.exports = keystore;
