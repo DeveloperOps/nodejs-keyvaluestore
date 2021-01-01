94a6bfdb8aba747354b1d71e81a4cc1e45877a2b
DeveloperOps <manidecp@gmail.com>
2021-01-01 14:58:55 +0530

    updated readme

--- SyncIndex.js
+++ SyncIndex.js
@@ -3,8 +3,10 @@ const synchronus = require('./syncKeystroe');
 synchronus.create("code1" , {code: "coding"} , 20000);
 synchronus.create("code2" , {code: "coding"} , 20000);
 synchronus.create("code3" , {code: "coding"} , 20000);
+
 synchronus.read("code1");
 synchronus.read("code2");
 synchronus.read("code3");
+
 synchronus.deletekey("code3");
 synchronus.deletekey("code1");
