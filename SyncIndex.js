d8937ebd43defe0bc959f1878afc25381b8c1fb4
DeveloperOps <manidecp@gmail.com>
2021-01-01 15:07:32 +0530

    checked syncindex.js

--- SyncIndex.js
+++ SyncIndex.js
@@ -4,9 +4,9 @@ synchronus.create("code1" , {code: "coding"} , 20000);
 synchronus.create("code2" , {code: "coding"} , 20000);
 synchronus.create("code3" , {code: "coding"} , 20000);
 
-// synchronus.read("code1");
-// synchronus.read("code2");
-// synchronus.read("code3");
+synchronus.read("code1");
+synchronus.read("code2");
+synchronus.read("code3");
 
-// synchronus.deletekey("code3");
-// synchronus.deletekey("code1");
+synchronus.deletekey("code3");
+synchronus.deletekey("code1");
